152dbff799eab58acc4a9be0d3da49ab632047b3
iRon7 <boderonald@gmail.com>
2022-04-07 14:31:20 +0200

    Changed warning parameter

--- HiddenString.ps1
+++ HiddenString.ps1
@@ -1,9 +1,7 @@
 class HiddenString {
     hidden [SecureString]$SecureString = [SecureString]::new()
-    hidden [Bool]$SuppressWarnings
     
-    hidden New([Object]$Object, [Bool]$SuppressWarnings) {
-        $This.SuppressWarnings = $SuppressWarnings
+    hidden New([Object]$Object, [Bool]$Warn) {
         if ($Object -is [SecureString]) { $This.SecureString = $Object }
         elseif ($Object -is [HiddenString]) { $This.SecureString = $Object.SecureString }
         elseif ($Object -is [Byte[]]) {
@@ -14,22 +12,24 @@ class HiddenString {
             }
             $This.SecureString = ConvertTo-SecureString $String
         }
-        elseif ($Object) {
-            if (!$This.SuppressWarnings) { Write-Warning 'For better obscurity, use a hidden or secure string for input.' }
-            $This.Add($Object)
-        }
+        elseif ($Object) { $This.Add($Object, $Warn) }
     }
-    HiddenString()                                   { $This.New($Null,   $False) }
-    HiddenString([Object]$Object)                    { $This.New($Object, $False) }
-    HiddenString([Object]$Object, $SuppressWarnings) { $This.New($Object, $SuppressWarnings) }
+    HiddenString()                       { $This.New($Null,   $True) }
+    HiddenString([Object]$Object)        { $This.New($Object, $True) }
+    HiddenString([Object]$Object, $Warn) { $This.New($Object, $Warn) }
     static [HiddenString]FromBase64Cypher([string]$String) { return [System.Convert]::FromBase64String($String) }
 
     [Void]Clear() { $This.SecureString.Clear() }
-    [Void]Add([Char[]]$Characters) { $Characters.ForEach{ $This.SecureString.AppendChar($_) } }
-    [Bool]Equals($Object) { return $This.SecureString.Equals($Object.SecureString) }
+    [Void]Add([Char[]]$Characters) { $This.Add($Characters, $True) }
+    [Void]Add([Char[]]$Characters, [bool]$Warn) {
+		if ($Warn -and $Characters.Count -gt 1) { Write-Warning 'For better obscurity, use a hidden or secure string for input.' }
+		$Characters.ForEach{ $This.SecureString.AppendChar($_) }
+	}
+    [Bool]Equals($Object) { return $This.SecureString.Equals($Object.SecureString)}
     [SecureString]ToSecureString() { return $This.SecureString }
-    [String]Reveal(){
-        if (!$This.SuppressWarnings) { Write-Warning 'For better obscurity, use a secure string output.' }
+    [String]Reveal() { $This.Reveal($True) }
+    [String]Reveal($Warn) {
+        if ($Warn) { Write-Warning 'For better obscurity, use a secure string output.' }
         $Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($This.SecureString)
         $String = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
         [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
